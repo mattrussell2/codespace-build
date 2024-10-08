a6e834d2988a3c636bffb1cfb8b90e11c2188706
matt russell <mrussell@cs.tufts.edu>
2024-10-08 09:56:49 -0400

    deploy script and more easystats packages

--- requirements-src.R
+++ requirements-src.R
@@ -1,10 +1,3 @@
-# options(repos = c(
-#     CRAN = 'https://packagemanager.rstudio.com/cran/__linux__/focal/latest'
-# ))
-
-# for some reason ARTool/FactoMineR installer fails to see emmeans with pak
-# install.packages(c('ARTool', 'FactoMineR'), dependencies=TRUE)
-
 pkgs <- c(    
     'ggplot2',
     'see',
@@ -14,7 +7,9 @@ pkgs <- c(
     'corrr',
     'ggcorrplot',
     'tidyverse',
-    'ez'
+    'ez', 
+    'report',
+    'modelbased',
 )
 
 if (!requireNamespace("pak", quietly = TRUE)) {
